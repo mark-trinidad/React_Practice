7185823dd932f42fae743dac793a79dc56311420
Mark Trinidad <marktrinidad40@gmail.com>
2024-11-26 23:03:33 +0300

    Completed the Blog

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -3,6 +3,8 @@ import Home from "./Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Create from "./Create";
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
+
 
 function App() {
   return (
@@ -13,7 +15,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/create" element={<Create />}></Route>
-            <Route path="/blogs/:id" element={<BlogDetails/>}></Route>
+            <Route path="/blogs/:id" element={<BlogDetails />}></Route>
+            <Route path="*" element={<NotFound/>}></Route>
           </Routes>
         </div>
       </div>
